544d19406a397c00dea989231895c684e3d1c3c9
Josh 'Jekotia' Ameli <contact@jekotia.net>
2018-02-09 12:53:35 -0500

    Updated setup for installing from a manually downloaded repo.

---
+++ shell-setup-local.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+# The lazy method. Will fix eventually
+cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cat shell-setup.sh | sudo bash -s $USER --local
--- shell-setup.sh
+++ shell-setup.sh
@@ -4,6 +4,16 @@
 origUser="$1"
 echo "origUser = $origUser"
 
+#if [[ "$origuser" == "" ]] ; then
+#	echo "ERROR: Unable to determine which user invoked this script"
+#	exit 1
+#elif [[ "$origUser" == "root" ]] ; then
+#	origHome="/root"
+#else
+#	origHome="/home/$origUser"
+#fi
+
+
 if which yum > /dev/null; then
 	pkgMgr="yum"
 elif which apt-get > /dev/null; then
@@ -14,9 +24,12 @@ fi
 
 $pkgMgr -y install wget curl nano zsh git bc
 
-echo --- Cloning \'.zsh\' from origin
-sudo -u $origUser git clone http://git.jekotia.net/jekotia/.zsh.git
-chown -R $origUser:$origUser /home/$origUser/.zsh
+if [[ ! "$2" == "--local" ]] ; then
+	cd $HOME
+	echo --- Cloning \'.zsh\' from origin
+	sudo -u $origUser git clone http://git.jekotia.net/jekotia/.zsh.git
+	chown -R $origUser:$origUser $origHome/.zsh
+fi
 
 if ( grep -Fxq "ZDOTDIR=\"\${ZDOTDIR:-\$HOME/.zsh}\"" "/etc/zshenv" || grep -Fxq "ZDOTDIR=\"\${ZDOTDIR:-\$HOME/.zsh}\"" "/etc/zsh/zshenv" ) ; then
 	echo --- WARNING: Did not update zshenv
@@ -32,8 +45,8 @@ else
 	fi
 fi
 
-cd /home/$origUser/.zsh/extras/powerline-fonts/
+cd $origHome/.zsh/extras/powerline-fonts/
 sudo -u $origUser sh ./install.sh
 
-cd /home/$origUser/.zsh/extras/
+cd $origHome/.zsh/extras/
 sudo -u $origUser sh ./awesome-terminal-fonts-setup.sh
