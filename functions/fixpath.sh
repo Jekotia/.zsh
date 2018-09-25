49c24b8244cde4a237550fda7a5a4090e6d92bd6
Josh 'Jekotia' Ameli <contact@jekotia.net>
2018-09-25 13:55:10 -0400

    Changed to lists and loops

--- functions/fixpath.sh
+++ functions/fixpath.sh
@@ -1,40 +1,70 @@
 #! /bin/bash
 function _fixpath () {
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/WINDOWS\/System32\/Wbem//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/WINDOWS\/System32\/WindowsPowerShell\/v1\.0//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/WINDOWS\/system32//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/WINDOWS//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/Users\/jekotia\/AppData\/Local\/Microsoft\/WindowsApps//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/Users\/jameli\/AppData\/Local\/Microsoft\/WindowsApps//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/Program Files\/ConEmu\/ConEmu\/Scripts//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/Program Files\/ConEmu\/ConEmu//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/Program\ Files\ (x86)\/P-CAD\ 2006\ Viewer//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/Program Files\/ConEmu//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/Program\ Files\ (x86)\/Windows\ Live\/Shared//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/Program\ Files\/Git\/cmd//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/Program\ Files\/Microsoft\ VS\ Code\/bin//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/Users\/jameli\/AppData\/Local\/atom\/bin//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/Users\/jameli\/AppData\/Local\/GitHubDesktop\/bin//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/Users\/jameli\/\.babun//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/Python27//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/Program\ Files\ \(x86\)\/Spiceworks\/Nmap//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/perl\/site\/bin//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/perl\/bin//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/StrawberryPerl\/c\/bin//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/StrawberryPerl\/perl\/bin//')
-	PATH=$(echo "$PATH" | sed -e 's/:\/cygdrive\/c\/StrawberryPerl\/perl\/site\/bin//')
-#	PATH=$(echo "$PATH" | sed -e 's/:path//')
-#	PATH=$(echo "$PATH" | sed -e 's/:path//')
-#	PATH=$(echo "$PATH" | sed -e 's/:path//')
+	declare -a addList=("/cygdrive/c/adb"
+	"/cygdrive/c/HashiCorp/Vagrant/bin"
+	)
+
+	declare -a replaceList=("derpingtoday"
+	"/cygdrive/c/perl/bin"
+	"/cygdrive/c/perl/site/bin"
+	"/cygdrive/c/ProgramData/chocolatey/bin"
+	"/cygdrive/c/Program\ Files/ConEmu"
+	"/cygdrive/c/Program\ Files/ConEmu/ConEmu"
+	"/cygdrive/c/Program\ Files/ConEmu/ConEmu/Scripts"
+	"/cygdrive/c/Program\ Files/Git/cmd"
+	"/cygdrive/c/Program\ Files/Microsoft\ VS\ Code/bin"
+	"/cygdrive/c/Program\ Files/Microsoft\ VS\ Code/bin"
+	"/cygdrive/c/Program\ Files\ (x86)/Admin\ Arsenal/PDQ\ Deploy"
+	"/cygdrive/c/Program\ Files\ (x86)/Admin\ Arsenal/PDQ\ Inventory"
+	"/cygdrive/c/Program\ Files\ (x86)/Microsoft\ SQL\ Server/100/Tools/Binn"
+	"/cygdrive/c/Program\ Files\ (x86)/Microsoft\ SQL\ Server/100/DTS/Binn"
+	"/cygdrive/c/Program\ Files\ (x86)/Nmap"
+	"/cygdrive/c/Program\ Files\ (x86)/P-CAD\ 2006\ Viewer"
+	"/cygdrive/c/Program\ Files\ (x86)/Spiceworks/Nmap"
+	"/cygdrive/c/Program\ Files\ (x86)/Spiceworks/Nmap"
+	"/cygdrive/c/Program\ Files\ (x86)/Windows\ Kits/10/Windows\ Performance\ Toolkit"
+	"/cygdrive/c/Program\ Files\ (x86)/Windows\ Kits/10/Microsoft\ Application\ Virtualization/Sequencer"
+	"/cygdrive/c/Program\ Files\ (x86)/Windows\ Live/Shared"
+	"/cygdrive/c/Python27"
+	"/cygdrive/c/StrawberryPerl/c/bin"
+	"/cygdrive/c/StrawberryPerl/perl/bin"
+	"/cygdrive/c/StrawberryPerl/perl/site/bin"
+	"/cygdrive/c/Users/jameli/\.babun"
+	"/cygdrive/c/Users/jameli/AppData/Local/atom/bin"
+	"/cygdrive/c/Users/jameli/AppData/Local/GitHubDesktop/bin"
+	"/cygdrive/c/Users/jameli/AppData/Local/Microsoft/WindowsApps"
+	"/cygdrive/c/Users/jameli/AppData/Local/Programs/Fiddler"
+	"/cygdrive/c/Users/jameli/AppData/Roaming/npm"
+	"/cygdrive/c/Users/jekotia/AppData/Local/Microsoft/WindowsApps"
+	"/cygdrive/c/WINDOWS"
+	"/cygdrive/c/WINDOWS/system32"
+	"/cygdrive/c/WINDOWS/System32/OpenSSH"
+	"/cygdrive/c/WINDOWS/System32/Wbem"
+	"/cygdrive/c/WINDOWS/System32/WindowsPowerShell/v1\.0"
+	)
+#	"/cygdrive/c/Program\ Files\ (x86)/"
 
 #/cygdrive/c/ProgramData/Oracle/Java/javapath
 #/cygdrive/c/Program Files/nodejs
 #/cygdrive/c/Program Files (x86)/Nmap
 #/cygdrive/c/Users/jameli/AppData/Roaming/npm
 
-#	PATH=$(echo "$PATH" | sed -e 's/:path//')
-	var="/cygdrive/c/adb" ; echo $PATH | grep "$var" > /dev/null 2&>1 || PATH="$PATH:$var"
-	var="/cygdrive/c/HashiCorp/Vagrant/bin" ; echo $PATH | grep "$var" > /dev/null 2&>1 || PATH="$PATH:$var"
+	for i in "${addList[@]}" ; do 
+		#var="/cygdrive/c/adb" ; echo $PATH | grep "$var" > /dev/null 2&>1 || PATH="$PATH:$var"
+		#var="/cygdrive/c/HashiCorp/Vagrant/bin" ; echo $PATH | grep "$var" > /dev/null 2&>1 || PATH="$PATH:$var"
+
+		if ! echo $PATH | grep "${i}" > /dev/null 2&>1 ; then
+			PATH="$PATH:${i}"
+		fi
+	done
+
+	for i in "${replaceList[@]}" ; do
+		#echo $i
+		## https://stackoverflow.com/a/29867033
+		PATH=$(echo "$PATH" | sed -e "s;\?:${i};;" -e "s;${i}:\?;;")
+	done
 
 	export PATH=$PATH
+#	echo $PATH
 }
+#_fixpath
