fe2b919ee64af068f6f03a790ce80b079912131a
Josh 'Jekotia' Ameli <contact@jekotia.net>
2019-09-04 08:41:13 -0400

    Install script for compiling from source

---
+++ install-zsh.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+if $(which yum) ; then
+	sudo yum update -y
+	sudo yum install -y git make ncurses-devel gcc autoconf man
+fi
+
+git clone -b zsh-5.7.1 https://github.com/zsh-users/zsh.git /tmp/zsh
+cd /tmp/zsh
+./Util/preconfig
+./configure
+sudo make -j 20 install.bin install.modules install.fns
